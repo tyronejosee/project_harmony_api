eb7b959e1b1e4fc6069c8573b8ffee66e55e61a8
Tyrone Jose <132106425+tyronejosee@users.noreply.github.com>
2024-07-05 14:42:38 -0400

    chore(project): add basic configs

---
+++ config/environment.py
@@ -0,0 +1,9 @@
+ENVIRONMENT = "development"
+# ENVIRONMENT = "production"
+
+SETTINGS_MODULE = "config.settings.development"
+
+if ENVIRONMENT == "development":
+    SETTINGS_MODULE = "config.settings.development"
+if ENVIRONMENT == "production":
+    SETTINGS_MODULE = "config.settings.production"
